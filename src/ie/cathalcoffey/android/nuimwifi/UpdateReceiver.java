5e9240c19cd27f5199a3b734deb7df0db3b48cae
Cathal Coffey <coffey.cathal@gmail.com>
2012-11-16 11:34:08 +0000

    1) Removed duplicate files 2) Set Activity style to Theme.Black.NoTitleBar.Fullscreen 3) Resized Button to match_parent 4) Updated version to versionName: 1.7 versionCode: 8

--- src/ie/cathalcoffey/android/nuimwifi/UpdateReceiver.java
+++ src/ie/cathalcoffey/android/nuimwifi/UpdateReceiver.java
@@ -3,13 +3,12 @@ package ie.cathalcoffey.android.nuimwifi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class UpdateReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (intent.getDataString().contains("ie.cathalcoffey.android"))
+		if (intent.getDataString().contains("ie.cathalcoffey.android.nuimwifi"))
 		{
 			Intent serviceIntent = new Intent(ForegroundService.ACTION_BACKGROUND);
 			serviceIntent.setClass(context, MyService.class);
